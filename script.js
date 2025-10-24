4622cc7c0bb1924d1ae5ad64b5788ef43f8fef02
Manh Quan <quangiang8989@gmail.com>
2025-10-24 16:21:10 +0700

    Clean version (no large files)

---
+++ script.js
@@ -0,0 +1,5 @@
+// Gửi form (giả lập)
+document.getElementById('contactForm').addEventListener('submit', e => {
+  e.preventDefault();
+  alert('Cảm ơn bạn đã gửi liên hệ! Mình sẽ phản hồi sớm nhất.');
+});
